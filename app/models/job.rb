4166e059aebca79bfa55d7518140ebb355261b12
Jerry Yu <jerryyu@uchicago.edu>
2015-07-01 16:49:19 -0500

    Added model validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ActiveRecord::Base
+	validates :title, presence: true
+	validates :job_id, presence: true, uniqueness: true
+	validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true
+
+	after_initialize :defaults
+
+	def defaults
+		self.is_admin = false if self.is_admin.nil?
+	end
 end
